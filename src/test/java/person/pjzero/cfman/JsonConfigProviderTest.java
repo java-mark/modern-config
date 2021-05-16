8e9459e6434fec21e20d66e76d413cf9533f32c9
pengjian05 <pengjian05@baidu.com>
2021-05-16 22:32:55 +0800

    build test environment

---
+++ src/test/java/person/pjzero/cfman/JsonConfigProviderTest.java
@@ -0,0 +1,13 @@
+package person.pjzero.cfman;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JsonConfigProviderTest {
+
+    @Test
+    void bind() {
+
+    }
+}
